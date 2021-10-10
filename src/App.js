6e7cd06eed550bf56d6464cb11d4ea9cf19a4846
Rahel Asmare <raheltibebu27@gmail.com>
2021-10-10 10:36:00 -0700

    React Week 2 Workshop Assignment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
-import { CAMPSITES } from './shared/campsites'
+import { CAMPSITES } from './shared/campsites';
+// import CampsiteInfo  from './components/CampsiteInfoComponent'
 
 class App extends Component {
     constructor(props){
@@ -20,6 +21,7 @@ class App extends Component {
                 </div>
                 </Navbar>
                 < Directory campsites={this.state.campsites} />
+                
             </div>
         );
     }

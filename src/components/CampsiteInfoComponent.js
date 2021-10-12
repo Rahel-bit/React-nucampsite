9ca24c0ff6c06abaa4192f88b1ec93b1c143421a
Rahel Asmare <raheltibebu27@gmail.com>
2021-10-12 10:24:52 -0700

    Adding Breadcrumb

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -31,7 +32,6 @@ function RenderCampsite({campsite}) {
                 <Card>
                 <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
-                    <CardTitle>{campsite.name}</CardTitle>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
                 </Card>
@@ -44,10 +44,20 @@ function RenderCampsite({campsite}) {
 function CampsiteInfo(props){
         if(props.campsite){
         return(
-        <div className="container">
+            <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 <RenderCampsite campsite={props.campsite} />
-                <RenderComments comment={props.campsite.comments} />
+                <RenderComments comments={props.comments} />
             </div>
         </div>
         );

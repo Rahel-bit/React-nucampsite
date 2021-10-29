4358312438707d6d72c2ff84c65b3d36bcf6a315
Rahel Asmare <raheltibebu27@gmail.com>
2021-10-29 08:38:31 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -13,7 +13,7 @@ const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -27,7 +27,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     ) 
                     })}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -52,7 +52,7 @@ function RenderComments({comments, addComment, campsiteId}) {
 
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.campsiteId,values.rating, values.author, values.text)
+            this.props.postComment(this.props.campsiteId,values.rating, values.author, values.text)
         }
     
         render(){
@@ -173,7 +173,7 @@ function CampsiteInfo(props){
             <RenderCampsite campsite={props.campsite} />
             <RenderComments 
                 comments={props.comments}
-                addComment={props.addComment}
+                postComment={props.postComment}
                 campsiteId={props.campsite.id}
             />
         </div>
